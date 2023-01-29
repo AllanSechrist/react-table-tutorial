500319fdeb5e9fb2b8f3699c6187f9a0d9fd9e21
AllanSechrist <sechristallan@gmail.com>
2023-01-29 11:29:18 +0900

    add columns and headers to table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import './App.css';
+import fakeData from "./MOCK_DATA.json"
+import {useMemo} from 'react'
+import {useTable} from 'react-table' 
 
 function App() {
+  const data = useMemo(() => fakeData, [])
+  const columns = useMemo(() => [
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+    {
+      Header: "First Name",
+      accessor: "first_name",
+    },
+    {
+      Header: "Last Name",
+      accessor: "last_name",
+    },
+    {
+      Header: "Email",
+      accessor: "email",
+    },
+    {
+      Header: "Gender",
+      accessor: "gender",
+    },
+    {
+      Header: "University",
+      accessor: "university",
+    },
+  ])
+
+
+
   return (
     <div className="App">
     </div>
