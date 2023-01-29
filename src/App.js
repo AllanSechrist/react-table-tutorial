37dba4c3eace5255d274f4e716da445f85137080
AllanSechrist <sechristallan@gmail.com>
2023-01-29 11:56:40 +0900

    Finish Tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import fakeData from "./MOCK_DATA.json"
-import {useMemo} from 'react'
-import {useTable} from 'react-table' 
+import "./App.css";
+import fakeData from "./MOCK_DATA.json";
+import { useMemo } from "react";
+import { useTable } from "react-table";
 
 function App() {
-  const data = useMemo(() => fakeData, [])
+  const data = useMemo(() => fakeData, []);
   const columns = useMemo(() => [
     {
       Header: "ID",
@@ -30,14 +30,40 @@ function App() {
       Header: "University",
       accessor: "university",
     },
-  ])
+  ], []);
 
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
 
-
-  return (
-    <div className="App">
+  return <div className="App">
+    <div className = "container">
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return(
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
-  );
+  </div>;
 }
 
 export default App;
